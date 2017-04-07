b7ae8cb23d15b67c7dcfc6799e4366968782d2e6
James Fairbanks <james.fairbanks@gtri.gatech.edu>
2017-04-07 00:11:21 -0400

    add ErrConnection type for cleaner interface to multiple queries.

--- featex/bulk.go
+++ featex/bulk.go
@@ -80,22 +80,47 @@ func Wrap(query string, kwargs BulkOptions) (string, error) {
 	return s, nil
 }
 
+// ErrConnection a connection with an error field so that you can make multiple
+// queries on the same connection without checking the error every time.
+// This is based on the "Errors are Values" blog post errWriter struct.
+type ErrConnection struct {
+	Conn *sql.DB
+	Err  error
+}
+
+// Query just like database/sql DB.Query but with built in error handling.
+func (ec *ErrConnection) Query(query string, args ...interface{}) *sql.Rows {
+	if ec.Err != nil {
+		return nil
+	}
+	rows, err := ec.Conn.Query(query, args...)
+	if err != nil {
+		ec.Err = err
+	}
+	return rows
+}
+
+// Query just like database/sql DB.Query but with built in error handling.
+func (ec *ErrConnection) QueryInt(query string, args ...interface{}) int {
+	var i int
+	if ec.Err != nil {
+		return i
+	}
+	i, err := QueryInt(ec.Conn, query, args...)
+	if err != nil {
+		ec.Err = err
+	}
+	return i
+}
+
 // BulkFeatures takes a select query and bulk query options wraps it in the BulkTemplate query
 // for inserting into the results table. It first creates a new job in the job table, so that you
 // can get the results by filtering the features table by the job_id
-func BulkFeatures(conn *sql.DB, query string, opt BulkOptions) (job_id int, err error) {
-	var bulk_query string
-	var countInsertions string
+func BulkFeatures(db *sql.DB, query string, opt BulkOptions) (job_id int, err error) {
+	// make queries
+	var bulk_query string      // the query that does bulk insertions
+	var countInsertions string // a query to count that we did insertions
 	bulk_query, err = Wrap(query, opt)
-	if err != nil {
-		return
-	}
-	job_id, err = NewJob(conn, opt, opt.Description)
-	if err != nil {
-		return
-	}
-	log.Printf("bulk_query: %s", bulk_query)
-	_, err = conn.Query(bulk_query, job_id)
 	if err != nil {
 		return
 	}
@@ -103,10 +128,24 @@ func BulkFeatures(conn *sql.DB, query string, opt BulkOptions) (job_id int, err 
 	if err != nil {
 		return
 	}
-	count, err := QueryInt(conn, countInsertions, job_id)
+
+	// Modify the database.
+	// Insert a new job into the job table
+	job_id, err = NewJob(db, opt, opt.Description)
 	if err != nil {
 		return
 	}
+	conn := ErrConnection{Conn: db}
+	log.Printf("bulk_query: %s", bulk_query)
+	// do the insertions
+	_ = conn.Query(bulk_query, job_id)
+	// count how many we did
+	count := conn.QueryInt(countInsertions, job_id)
+	err = conn.Err
+	if err != nil {
+		return
+	}
+	// check that there were insertions.
 	var c int = 1
 	if count < c {
 		err = fmt.Errorf("Insertion did not yield any values")
